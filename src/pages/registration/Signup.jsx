f60c11b2f60c87882bdb902f0844ae5f19b5a294
Abhishek Sonawane <109890906+abhishekSonawane97@users.noreply.github.com>
2024-03-29 01:55:19 +0530

    Update Signup.jsx

--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -82,7 +82,7 @@ const Signup = () => {
             </div>
 
             <div>
-                <h2 className="text-white">Already have an account ? <Link className='text-red-500 font-bold' to={'/signup'}> Login </Link>
+                <h2 className="text-white">Already have an account ? <Link className='text-red-500 font-bold' to={'/login'}> Login </Link>
                 </h2>
             </div>
 
